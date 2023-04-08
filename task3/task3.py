08c6d1c9093f61c5c1aed460677741ff6d8695a0
MrGRollins <mrvalve03@mail.ru>
2023-04-08 06:15:35 +0300

    added task3

---
+++ task3/task3.py
@@ -0,0 +1,116 @@
+# 3.Создать иерархию классов фигур: квадрат, прямоугольник, треугольник, круг.
+#   Каждый класс должен реализовывать следующие методы:
+#   •	вычисление площади
+#   •	вычисление периметра
+#   •	сравнение площади с другой фигурой (больше или меньше)
+#   •	сравнение периметра с другой фигурой (больше или меньше)
+
+import math
+from abc import ABC
+
+# Figure
+class Figure(ABC):
+    def __init__(self):
+        self.perimeter = 0
+        self.square = 0
+
+    def new_parameters(self):
+        pass
+
+    def new_perimeter(self):
+        pass
+
+    def new_square(self):
+        pass
+
+    def perimeter_info(self, other):
+        return self.perimeter < other.new_perimeter()
+
+    def square_info(self, other):
+        return self.square < other.new_square()
+
+# Прямоугольник
+class Rectangle(Figure):
+    def __init__(self, width: float, height: float):
+        super().__init__()
+        assert width > 0 and height > 0, "The rectangle exists"
+        self._width = width
+        self._height = height
+
+    def new_parameters(self):
+        return self._width, self._height
+
+    def new_perimeter(self):
+        self.perimeter = (self._width + self._height) * 2
+        return self.perimeter
+
+    def new_square(self):
+        self.square = self._width * self._height
+        return self.square
+
+# Квадрат
+class Square(Rectangle):
+    def __init__(self, a: float):
+        super().__init__()
+        self.a = a
+
+    def new_parameters(self):
+        return self.a
+
+    def new_perimeter(self):
+        self.perimeter = self.a * 4
+        return self.perimeter
+
+    def new_square(self):
+        self.square = self.a * self.a
+        return self.square
+
+# Круг
+class Circle(Figure):
+    def __init__(self, r: float):
+        super().__init__()
+        self.r = r
+
+    def new_parameters(self):
+        return self.r
+
+    def new_perimeter(self):
+        self.perimeter = 2 * math.pi * self.r
+        return self.perimeter
+
+    def new_square(self):
+        self.square = math.pi * math.sqrt(self.r)
+        return self.square
+
+# Треугольник
+class Triangle(Figure):
+    def __init__(self, a: float, b: float, c: float):
+        super().__init__()
+
+        assert a > 0 and b > 0 and c > 0, "The triangle exist"
+        assert a < b + c and b < a + c and c < a + b, "The triangle exist"
+
+        self.a = a
+        self.b = b
+        self.c = c
+
+    def new_parameters(self):
+        return self.a, self.b, self.c
+
+    def new_perimeter(self):
+        self.perimeter = self.a + self.b + self.c
+        return self.perimeter
+
+    def new_square(self):
+        h_per = self.perimeter/2
+        self.square = math.sqrt(h_per * (h_per - self.a) * (h_per - self.b) * (h_per - self.c))
+        return self.square
+
+if __name__ == '__main__':
+    fig_rec = Rectangle(4.5, 7.1)
+    print(
+        '\n',
+        "Parameters figure:  \n",
+          " • Perimeter: ", fig_rec.new_square(), '\n',
+          " • Square: ", fig_rec.new_perimeter(), '\n'
+    )
